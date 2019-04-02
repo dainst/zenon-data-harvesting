2e7b8ad21988af043f57ab945398315ef119273f
Helena Nebel <Helena.Nebel@dainst.de>
2019-04-02 18:34:17 +0200

    Second version of e-periodica.ch-harvesting-script

--- OAI-PMH_test_2.py
+++ OAI-PMH_test_2.py
@@ -1,5 +1,6 @@
 import json
 import re
+import time
 from sickle import Sickle
 import requests
 sickle = Sickle('https://www.e-periodica.ch/oai/dataprovider') # initialisiert Verbindung
@@ -8,6 +9,7 @@ record_identifiers={}
 doi_list=[]
 item_number=0
 record=0
+print(time.clock())
 for record in records_900:
     if item_number > 9:
         break
@@ -17,13 +19,21 @@ for record in records_900:
     doi_list.append(doi)
     item_number+=1
     record_identifiers["records_900"] = doi_list
+print(time.clock())
 record_number=1
 for doi in record_identifiers["records_900"]:
     sickle2 = Sickle('https://www.e-periodica.ch/oai/dataprovider')
-    text=str(sickle2.GetRecord(identifier=doi, metadataPrefix = 'oai_dc'))
-    filename='record_nr' + str(record_number) + '.xml'
-    with open(filename, 'w') as file:
-        file.write(text)
+    content_list=sickle2.GetRecord(identifier=doi, metadataPrefix = 'oai_dc')
+    print(type(content_list))
+    import xml.etree.ElementTree as ET
+
+    for child in content_list:
+        if child[1][0] is not None:
+            print(f"Element: {child[0]}, content: {child[1]}")
+
+    #filename='record_nr' + str(record_number) + '.xml'
+    #with open(filename, 'w') as file:
+        #file.write(text)
     record_number+=1
 from pymarc import Record, Field
 record = Record()
