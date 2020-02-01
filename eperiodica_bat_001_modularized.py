87a7a8f18e363d3a826eefdd1f04c7c7572cfd7b
helenanebel <Helena.nebel@dainst.de>
2020-02-01 18:20:15 +0100

    Current version of script for harvesting BAT

---
+++ eperiodica_bat_001_modularized.py
@@ -0,0 +1,6 @@
+import eperiodica_modularized
+
+
+def harvest(path):
+    return_string = eperiodica_modularized.harvest_eperiodica(path, 'bat-001', 'Associazione Archeologica Ticinese', 'Lugano', 'ita', 3, '001543081', 'Bollettino dell’Associazione Archeologica Ticinese', 'ar p o||||||   a|')
+    return return_string
