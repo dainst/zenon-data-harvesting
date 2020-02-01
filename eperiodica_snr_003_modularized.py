0d620ab1a02ef06cb61865442fdb84da332dd30c
helenanebel <Helena.nebel@dainst.de>
2020-02-01 18:20:43 +0100

    Current version of script for harvesting SNR

---
+++ eperiodica_snr_003_modularized.py
@@ -0,0 +1,5 @@
+import eperiodica_modularized
+
+def harvest(path):
+    return_string = eperiodica_modularized.harvest_eperiodica(path, 'snr-003', 'Schweizerische Numismatische Gesellschaft', 'Bern', 'ger', 3, '001570578', 'Schweizerische numismatische Rundschau', 'ar p o||||||   a|')
+    return return_string
