e1066ec9f02e6283d829b5e53dcbecd05d7b6b6c
helenanebel <Helena.nebel@dainst.de>
2020-02-01 18:19:41 +0100

    Current version script for harvesting AKB

---
+++ eperiodica_akb_002_modularized.py
@@ -0,0 +1,5 @@
+import eperiodica_modularized
+
+def harvest(path):
+    return_string = eperiodica_modularized.harvest_eperiodica(path, 'akb-002', 'Archäologischer Dienst des Kantons Bern', 'Bern', 'ger', 2, '000855529', 'Archäologie Bern', 'ar p o||||||   a|')
+    return return_string
