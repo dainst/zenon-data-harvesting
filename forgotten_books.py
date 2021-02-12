c70c3cf241b7943aab300dab291df1f3e07a41d3
helenanebel <Helena.nebel@dainst.de>
2021-02-12 22:58:38 +0100

    Filter invalid links

--- forgotten_books.py
+++ forgotten_books.py
@@ -1,12 +1,9 @@
-import urllib.parse, urllib.request
-import urllib.parse, urllib.request
 from bs4 import BeautifulSoup
 import json
 import write_error_to_logfile
 from harvest_records import harvest_records
 import os
-import gzip
-from pymarc import MARCReader
+from create_new_record import link_is_valid
 
 
 def create_publication_dicts(last_item_harvested_in_last_session, *other):
@@ -23,6 +20,17 @@ def create_publication_dicts(last_item_harvested_in_last_session, *other):
                 publication_xml = open('gai_metadata/' + publication_file, 'r')
                 pub_string = publication_xml.read()
                 publication_soup = BeautifulSoup(pub_string, features="lxml").find('product')
+                publication_ids = [tag.find('idvalue').text for tag in publication_soup.find_all('productidentifier') if tag.find('productidtype').text == '01']
+                publication_dict['html_links'] = ['https://www.forgottenbooks.com/en/books/' + id for id in publication_ids]
+                publication_dict['pdf_links'] = ['https://www.forgottenbooks.com/en/download/' + id + '.pdf' for id in publication_ids]
+                unvalid_link = False
+                for link in publication_dict['html_links'] + publication_dict['pdf_links']:
+                    if not link_is_valid(link):
+                        unvalid_link = True
+                        break
+                if unvalid_link:
+                    print('found invalid link:', publication_dict['html_links'] + publication_dict['pdf_links'])
+                    continue
                 publication_dict['title_dict']['main_title'] = publication_soup.find('title').find('titletext').text.replace(' (Classic Reprint)', '')
                 publication_dict['default_language'] = \
                     publication_soup.find('languagecode').text.lower()
@@ -45,9 +53,6 @@ def create_publication_dicts(last_item_harvested_in_last_session, *other):
                     publication_dict['additional_fields'].append({'tag': '300', 'indicators': [' ', ' '], 'subfields': ['a', '1 online ressource , ' + publication_soup.find('numberofpages').text + ' pp.', 'b', 'illustrations'], 'data': ''})
                 else:
                     publication_dict['additional_fields'].append({'tag': '300', 'indicators': [' ', ' '], 'subfields': ['a', '1 online ressource , ' + publication_soup.find('numberofpages').text + ' pp.'], 'data': ''})
-                publication_ids = [tag.find('idvalue').text for tag in publication_soup.find_all('productidentifier') if tag.find('productidtype').text == '01']
-                publication_dict['html_links'] = ['https://www.forgottenbooks.com/en/books/' + id for id in publication_ids]
-                publication_dict['pdf_links'] = ['https://www.forgottenbooks.com/en/download/' + id + '.pdf' for id in publication_ids]
                 publication_dict['terms_of_use_and_reproduction'] = \
                     {'terms_note':
                          'The e-books may be copied or printed for personal or educational use only. '
@@ -55,16 +60,6 @@ def create_publication_dicts(last_item_harvested_in_last_session, *other):
                      'use_and_reproduction_rights': '', 'terms_link': 'https://www.forgottenbooks.com/de/terms'}
                 publication_dict['additional_fields'].append({'tag': '650', 'indicators': [' ', '7'], 'subfields': ['a', publication_soup.find('basicmainsubject').text, '2', 'bisacsh'], 'data': ''})
                 # https://bisg.org/page/History
-                try:
-                    with urllib.request.urlopen(publication_dict['html_links'][0]) as f:
-                        html = gzip.open(f)
-                        webpage_soup = BeautifulSoup(html.read(), 'html.parser')
-                        info = webpage_soup.find('meta', attrs={'name':'description'})['content']
-                        # publication_dict['additional_fields'].append({'tag': '698', 'indicators': [' ', ' '], 'subfields': ['a', info], 'data': ''})
-                except Exception as e:
-                    # publication_dict['additional_fields'].append({'tag': '698', 'indicators': [' ', ' '], 'subfields': ['a', publication_dict['html_links'][0]], 'data': ''})
-                    write_error_to_logfile.write(e)
-                    print(publication_dict['html_links'])
                 publication_dict['publication_etc_statement']['publication'] = {'place': 'London', 'responsible': 'Forgotten Books', 'country_code': 'enk'}
                 publication_dict['publication_year'] = publication_soup.find('publicationdate').text[:4]
                 publication_dicts.append(publication_dict)
