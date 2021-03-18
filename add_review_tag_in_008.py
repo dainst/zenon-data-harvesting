85ec07dc11806f387b2fb0e0380446956c78c2bd
helenanebel <Helena.nebel@dainst.de>
2021-03-18 23:17:19 +0100

    Update records in Koha

---
+++ add_review_tag_in_008.py
@@ -0,0 +1,21 @@
+import csv
+import urllib.request
+from pymarc import MARCReader
+
+nr = 0
+out = open('records/reviews/substitution_records.mrc', 'wb')
+with open('Rezensionen-reportresults.csv', "r") as record_table:
+    reader = csv.reader(record_table, delimiter=';')
+    row_nr = 0
+    for row in reader:
+        row_nr += 1
+        if row_nr > 1:
+            record_id = row[0]
+            try:
+                webfile = urllib.request.urlopen("https://zenon.dainst.org/Record/" + record_id + "/Export?style=MARC")
+                new_reader = MARCReader(webfile, force_utf8=True)
+                for file in new_reader:
+                    out.write(file.as_marc21())
+                    nr += 1
+            except Exception as e:
+                print(e)
---
+++ check_links.py
@@ -0,0 +1,38 @@
+import urllib.request
+import csv
+from pymarc import MARCReader, Field
+from urllib.error import HTTPError
+
+other_problems = []
+nr = 0
+out = open('records/check_links/substitution_records.mrc', 'wb')
+with open('856-reportresults.csv', "r") as record_table:
+    reader = csv.reader(record_table, delimiter=';')
+    row_nr = 0
+    for row in reader:
+        row_nr += 1
+        if row_nr > 1:
+            record_id = row[0]
+            invalid_link = False
+            for identifier in row[2].split():
+                if identifier[0] in ['h', 'w']:
+                    if identifier[0:3] == 'www':
+                        identifier = 'http://' + identifier
+                    try:
+                        webfile = urllib.request.urlopen(identifier)
+                    except HTTPError as err:
+                        if err.code == 404:
+                            webfile = urllib.request.urlopen("https://zenon.dainst.org/Record/" + record_id + "/Export?style=MARC")
+                            new_reader = MARCReader(webfile, force_utf8=True)
+                            for file in new_reader:
+                                if nr % 250 == 0:
+                                    out = open('records/check_links/records_to_check' + str(nr) + '.mrc', 'wb')
+                                out.write(file.as_marc21())
+                                nr += 1
+                    except Exception as e:
+                        print(e)
+                        print('invalid but not 404', identifier)
+                        other_problems.append(record_id)
+
+print(row_nr)
+print(nr)
